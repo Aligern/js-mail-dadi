74667d7b918532dfbf21e0afc967befb4ef1b12c
Aligern <sasso.luca93@gmail.com>
2024-03-11 18:24:13 +0100

    email done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 // email check 
-const button = document.querySelector('btn btn-outline-dark');
+const button = document.querySelector('.btn.btn-outline-dark');
 console.log(button)
 
 button.addEventListener('click', function(){
+    let exist = false;
+    console.log(exist);
     console.log('Sto cercando la tua mail');
     const email = document.getElementById('emailinput').value;
     console.log(email);
@@ -14,10 +16,24 @@ button.addEventListener('click', function(){
         "email-4@gmail.com" // 3
 ];
     console.log(mailDatabase.length);
+    // this for cicle is for the array search
     for (let i = 0; i < mailDatabase.length; i++) {
         console.log(mailDatabase[i]);
-        let mailAddress = mailDatabase[i];
-        console.log(mailAddress);
+        if (mailDatabase[i] === email) {
+            exist = true;
+    }   
+}   
+let output = document.getElementById('output');
+console.log(output);
+    if (exist) {
+        output.innerHTML = 'Puoi accedere'
+    }   else {
+        output.innerHTML = 'Indirizzo mail non trovato'
     }
-}
-);
+});
+
+
+// dice game
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
