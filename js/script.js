a56485b9d63f5c97f0472e6ca17abd3793363e15
Aligern <sasso.luca93@gmail.com>
2024-03-11 18:53:08 +0100

    added dice imgs

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 // email check 
-const button = document.querySelector('.btn.btn-outline-dark');
-console.log(button)
 
+// this variable is our button
+const button = document.querySelector('.btn.btn-outline-dark');
+console.log(button);
+
+
+// here we explain the machine how to do the check
 button.addEventListener('click', function(){
+    // this variable indicates the false value of the email
     let exist = false;
     console.log(exist);
     console.log('Sto cercando la tua mail');
+    // here we get the input from the user
     const email = document.getElementById('emailinput').value;
     console.log(email);
-    // email array 
+    // email array that we will use as database to do the check
     const mailDatabase = [
         "email-1@gmail.com",// 0
         "email-2@gmail.com",// 1
@@ -23,6 +29,7 @@ button.addEventListener('click', function(){
             exist = true;
     }   
 }   
+    // here we explain the machine what to do after the check
 let output = document.getElementById('output');
 console.log(output);
     if (exist) {
@@ -34,6 +41,28 @@ console.log(output);
 
 
 // dice game
+// this function is needed to randomize
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
+};
+
+const button2 = document.querySelector('.btn.btn-outline-primary');
+console.log(button2);
+// here we explain the machine what to do after the click
+button2.addEventListener('click', function(){
+    let diceImg = document.getElementById('dice-img');
+    console.log(diceImg);
+    // here we specify how the dices should work by attributing them the randomizer function
+    let playerDice = getRndInteger(1,6);
+    console.log(playerDice);
+    let pcDice = getRndInteger(1,6);
+    console.log(pcDice);
+    if (playerDice === pcDice) {
+        
+        console.log('Hai pareggiato');
+    } else if (playerDice < pcDice) {
+        console.log('hai perso! =( ');
+    } else {
+        console.log('hai vinto!') 
     }
+});
